7d44bb48b02883893fc78a3eead35e59f32731f4
Johan Cedborger <jced@mac.lan>
2025-01-26 23:34:12 +0100

    privacy policy

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection, defineEntry } from "astro:content";
 
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
@@ -11,10 +11,18 @@ const blogCollection = defineCollection({
       src: z.string(),
       alt: z.string(),
     }),
-    publishDate: z.string().transform(str => new Date(str)),
-    author: z.string().default('Astroship'),
+    publishDate: z.string().transform((str) => new Date(str)),
+    author: z.string().default("Astroship"),
     category: z.string(),
     tags: z.array(z.string()),
+  }),
+});
+
+const privacyPolicyCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    publishDate: z.string().transform((str) => new Date(str)),
+    author: z.string().default("Johan Cedborger"),
   }),
 });
 
@@ -27,13 +35,14 @@ const teamCollection = defineCollection({
       src: z.string(),
       alt: z.string(),
     }),
-    publishDate: z.string().transform(str => new Date(str)),
+    publishDate: z.string().transform((str) => new Date(str)),
   }),
 });
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'blog': blogCollection,
-  'team': teamCollection,
+  blog: blogCollection,
+  team: teamCollection,
+  "privacy-policy": privacyPolicyCollection,
 };
